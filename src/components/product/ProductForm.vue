4d7876558c3e38a89fef79d36776141d8372b3d0
David <daveo1001@gmail.com>
2017-11-13 12:56:12 -0600

    Manufacturer select not auto populating
    updating the model to the id and removing extra code from option allows option to be selected automatically.

--- src/components/product/ProductForm.vue
+++ src/components/product/ProductForm.vue
@@ -30,12 +30,12 @@
         <select
           type="text"
           class="form-control"
-          v-model="model.manufacturer"
+          v-model="model.manufacturer._id"
           v-validate="'required'"
           name="manufacturer"
           :class="{'form-control': true, 'error': errors.has('manufacturer') }">
           <template v-for="manufacturer in manufacturers">
-            <option :value="manufacturer._id" :selected="manufacturer._id == (model.manufacturer && model.manufacturer._id)">{{manufacturer.name}}</option>
+            <option v-bind:key="manufacturer._id" :value="manufacturer._id">{{manufacturer.name}}</option>
           </template>
         </select>
         <span class="small text-danger" v-show="errors.has('manufacturer')">Manufacturer is required</span>
